104a792f6920f28a23719e6a0d96c56298599d93
Artem Pasyshen <reyogoapp@gmail.com>
2020-03-31 15:40:25 +0300

    fix spaces

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -6,65 +6,65 @@ import {getClassName, formatImgUrl, modifyArticle} from '../../helpers/index';
 import {saveNewTitle, togglePopupWindow} from '../../redux/actions/index';
 
 const ArticleCard = ({options, rowIndex, columnIndex}) => {
-	const dispatch = useDispatch();
-	let additionalInfo = useSelector(state => state.articles.additionalInfo);
-	const {width, url, title, imageUrl, hide} = options;
-	const ref = useRef();
-	const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
-	const [editable, setEditable] = useState(false);
-	const [articleTitle, setArticleTitle] = useState(title);
-	useEffect(() => {
-		if (ref.current) {
-			setDimensions({
-				width: ref.current.offsetWidth,
-				height: ref.current.offsetHeight
-			});
-		}
-	}, []);
-	const handleEdit = () => setEditable(!editable);
-	const handleChange = (e) => setArticleTitle(e.target.value);
-	const saveHandler = () => {
-		dispatch(saveNewTitle(articleTitle, rowIndex, columnIndex));
-		return handleEdit();
-	};
-	const handleDelete = () => {
-		dispatch(togglePopupWindow(modifyArticle('HIDE', additionalInfo, rowIndex, columnIndex )));
-	};
-	return (
-		<>
-		{ !hide && (
-		  <article ref={ref} className={getClassName(width)}>
-			  <img src={formatImgUrl(imageUrl, dimensions)} alt={title} />
-			  { editable ? (
-				  <input type="text" value={articleTitle} onChange={handleChange}/>
-			  ) : (
-				  <a target="blank" href={url}>
-					  <span>{title}</span>
-				  </a>
-			  )
-			  }
-			  <div className="action-wrapper">
-				  <button className="edit-btn" onClick={!editable ? handleEdit : saveHandler}>
-					  {!editable ? 'Edit' : 'Save'}
-				  </button>
-				  <button className="delete-btn" onClick={handleDelete}>Delete</button>
-			  </div>
-		  </article>
-	  )}
-		</>
-	);
+  const dispatch = useDispatch();
+  let additionalInfo = useSelector(state => state.articles.additionalInfo);
+  const {width, url, title, imageUrl, hide} = options;
+  const ref = useRef();
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const [editable, setEditable] = useState(false);
+  const [articleTitle, setArticleTitle] = useState(title);
+  useEffect(() => {
+    if (ref.current) {
+      setDimensions({
+        width: ref.current.offsetWidth,
+        height: ref.current.offsetHeight
+      });
+    }
+  }, []);
+  const handleEdit = () => setEditable(!editable);
+  const handleChange = (e) => setArticleTitle(e.target.value);
+  const saveHandler = () => {
+    dispatch(saveNewTitle(articleTitle, rowIndex, columnIndex));
+    return handleEdit();
+  };
+  const handleDelete = () => {
+    dispatch(togglePopupWindow(modifyArticle('HIDE', additionalInfo, rowIndex, columnIndex )));
+  };
+  return (
+    <>
+    { !hide && (
+      <article ref={ref} className={getClassName(width)}>
+        <img src={formatImgUrl(imageUrl, dimensions)} alt={title} />
+        { editable ? (
+          <input type="text" value={articleTitle} onChange={handleChange}/>
+        ) : (
+          <a target="blank" href={url}>
+            <span>{title}</span>
+          </a>
+        )
+        }
+        <div className="action-wrapper">
+          <button className="edit-btn" onClick={!editable ? handleEdit : saveHandler}>
+            {!editable ? 'Edit' : 'Save'}
+          </button>
+          <button className="delete-btn" onClick={handleDelete}>Delete</button>
+        </div>
+      </article>
+    )}
+    </>
+  );
 };
 
 ArticleCard.propTypes = {
-	options: PropTypes.shape({
-		type: PropTypes.string,
-		width: PropTypes.number,
-		url: PropTypes.string,
-		title: PropTypes.string,
-		imageUrl: PropTypes.string,
-	}),
-	rowIndex: PropTypes.number,
-	columnIndex: PropTypes.number
+  options: PropTypes.shape({
+    type: PropTypes.string,
+    width: PropTypes.number,
+    url: PropTypes.string,
+    title: PropTypes.string,
+    imageUrl: PropTypes.string,
+  }),
+  rowIndex: PropTypes.number,
+  columnIndex: PropTypes.number
 };
 
 export default ArticleCard;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,24 +5,24 @@ import {togglePopupWindow} from '../../redux/actions/index';
 import {modifyArticle} from '../../helpers/index';
 
 const Modal = () => {
-	const dispatch = useDispatch();
-	const additionalInfo = useSelector(state => state.articles.additionalInfo);
-	let timer;
-	const deleteArticle = () => timer = setTimeout(() =>
-			dispatch(togglePopupWindow(modifyArticle('DELETE', additionalInfo)))
-		, 3000);
-	useEffect(() => {
-		deleteArticle();
-	}, [deleteArticle]);
-	const restoreHandler = () => {
-		clearTimeout(timer);
-		return dispatch(togglePopupWindow(modifyArticle('RESTORE', additionalInfo)));
-	};
-	return (
-		<div className="modal">
-		  <p>Do you want to restore article?</p>
-		  <button onClick={restoreHandler}>Yes!</button>
-	  </div>
-	);
+  const dispatch = useDispatch();
+  const additionalInfo = useSelector(state => state.articles.additionalInfo);
+  let timer;
+  const deleteArticle = () => timer = setTimeout(() =>
+      dispatch(togglePopupWindow(modifyArticle('DELETE', additionalInfo)))
+    , 3000);
+  useEffect(() => {
+    deleteArticle();
+  }, [deleteArticle]);
+  const restoreHandler = () => {
+    clearTimeout(timer);
+    return dispatch(togglePopupWindow(modifyArticle('RESTORE', additionalInfo)));
+  };
+  return (
+    <div className="modal">
+      <p>Do you want to restore article?</p>
+      <button onClick={restoreHandler}>Yes!</button>
+    </div>
+  );
 };
 export default Modal;
